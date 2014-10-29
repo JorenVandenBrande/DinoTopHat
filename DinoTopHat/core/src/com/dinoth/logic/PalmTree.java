0913700c2acaf9924166fd2cb3718dce41bf538c
JorenVandenBrande <joren_vdb@hotmail.be>
2014-10-29 14:16:13 +0100

    updated sprites

--- DinoTopHat/core/src/com/dinoth/logic/PalmTree.java
+++ DinoTopHat/core/src/com/dinoth/logic/PalmTree.java
@@ -7,7 +7,7 @@ public class PalmTree extends Entity{
 
 	public PalmTree(Texture tex,Rectangle rect){
 		super(tex,rect, 80, 70, -30, 0, 380);
-		this.updateHitBoxDim(20, 50);
+		this.updateHitBoxDim(15, 50);
 		this.isGood=false;
 	}
 }
